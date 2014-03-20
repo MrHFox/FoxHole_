b62069cf925b7cdb6ff2647590bc4eb690142bf6
CoinFox <foxcoindev@gmail.com>
2014-03-21 05:39:37 +1300

    Fixed age-old RPC bug.

--- src/foxcoinrpc.cpp
+++ src/foxcoinrpc.cpp
@@ -187,7 +187,7 @@ Object blockToJSON(const CBlock& block, const CBlockIndex* blockindex)
     result.push_back(Pair("time", (boost::int64_t)block.GetBlockTime()));
     result.push_back(Pair("nonce", (boost::uint64_t)block.nNonce));
     result.push_back(Pair("bits", HexBits(block.nBits)));
-    result.push_back(Pair("Hardness:", getHardness()));
+    result.push_back(Pair("difficulty", getHardness()));
 
     if (blockindex->pprev)
         result.push_back(Pair("previousblockhash", blockindex->pprev->GetBlockHash().GetHex()));
